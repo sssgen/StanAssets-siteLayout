da9ffbdcf7010c6fa74ff47ba8d3e020bcebfa21
sssgen <118835027+sssgen@users.noreply.github.com>
2023-02-05 18:57:43 +0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,33 @@
+function menuBar(x) {
+    x.classList.toggle("change");
+}
+
+const topMenu = document.getElementById('menu');
+const logoMenu = document.getElementById('logoMenu');
+const buttonMenu = document.getElementById('linkMenu');
+const links = document.getElementsByClassName('menu__link')
+
+
+window.addEventListener('scroll', () => {
+    let scrollDistance = window.scrollY;
+    links[0].style.color = 'blue';
+
+    if(scrollDistance >= 200){
+        topMenu.classList.add('menu__color');
+        logoMenu.src = "style/img/menu__Logo-scroll.png"
+        for (const link of links) {
+            link.style.color = 'blue';
+        }
+    } else {
+        topMenu.classList.remove('menu__color');
+        logoMenu.src = "style/img/menu__Logo.png"
+        for (const link of links) {
+            link.style.color = 'white';
+        }
+    }
+    
+})
+
+function phoneMenu() {
+    document.getElementById("phoneMenu").classList.toggle("show");
+}
